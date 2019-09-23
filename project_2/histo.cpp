5d5d58299d9aec3be87c2470163b70c5e603c476
Samarth Dave <samarthdave@users.noreply.github.com>
2019-09-23 17:00:35 -0500

    Add file IO, calculate basic stats (std_dev, mean, range)

--- project_2/histo.cpp
+++ project_2/histo.cpp
@@ -1,19 +1,79 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-int main(int argc, char** argv) {
+// Standard deviation definition
+float calculateStandardDeviation(vector<int> &items, float mean);
 
-  if (argc!= 2) {
+int main(int argc, char** argv) {
+  if (argc != 2) {
     cout << "Invalid number of inputs. Usage is \"histo FILENAME\"" << endl;
     exit(1);
   }
 
+  ifstream input;
+
+  // argv[0] is program name
+  // 1 is file name
   string filename = argv[1];
+  input.open(filename);
 
   // read file and loop over contents to calculate mean, std-dev, etc
+  int count = 0;
+  int sum = 0;
+  int min = -1, max = -1;
+  vector<int> temperatures;
 
+  int num;
+  // append all the values ot the temps vector
+  while (input >> num) {
+    if (min == -1) min = num;
+    if (max == -1) max = num;
+    // comparisons for max and min
+    if (num > max) {
+      max = num;
+    }
+    if (num < min) {
+      min = num;
+    }
+    sum += num;
+    count += 1;
+    temperatures.push_back(num);
+  }
+
+  float average = (float) sum / count;
+  float stdDev = calculateStandardDeviation(temperatures, average);
+
+  cout << "------------" << endl;
+  cout << "count: " << count << endl;
+  cout << "range: " << min << " - " << max << endl;
+  cout << "mean: " << average << endl;
+  cout << "std_dev: " << stdDev << endl;
+  cout << "------------" << endl;
+
+  // close the stream
+  input.close();
   return 0;
 
+}
+
+float calculateStandardDeviation(vector<int> &items, float mean) {
+  int itemCount = items.size();
+  
+  if (itemCount == 1) {
+    cout << "ERROR: The standard deviation for 1 item does not exist. Returning -1.0" << endl;
+    return -1.0;
+  }
+
+  float sumOfDifferences = 0;
+  
+  for (int i = 0; i < itemCount; i++) {
+    sumOfDifferences += pow(float(items[i]) - mean, 2);
+  }
+
+  return sqrt((float)1/(itemCount-1) * sumOfDifferences);
 }
