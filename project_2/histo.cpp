f1d8cd9d02741871e3eb76b002afcf618de538bb
Samarth Dave <samarthdave@users.noreply.github.com>
2019-09-27 01:51:34 -0500

    Project 2: good project but I guess we learned a lesson about procrastination

--- project_2/histo.cpp
+++ project_2/histo.cpp
@@ -3,11 +3,14 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 // Standard deviation definition
-float calculateStandardDeviation(vector<int> &items, float mean);
+double calculateStandardDeviation(vector<double> &items, double mean);
+// fucntion to print entire set
+void printHistogram(vector<double> &items, double min, double max);
 
 int main(int argc, char** argv) {
   if (argc != 2) {
@@ -24,20 +27,22 @@ int main(int argc, char** argv) {
 
   // read file and loop over contents to calculate mean, std-dev, etc
   int count = 0;
-  int sum = 0;
+  double sum = 0;
 
   // use a boolean to ensure correct min, max values
   // instead of assuming -1
   bool firstRun = true;
-  int min, max;
-  vector<int> temperatures;
+  double min, max;
+  vector<double> temperatures;
 
-  int num;
-  // append all the values ot the temps vector
+  double num;
+  // append all the values to the temps vector
   while (input >> num) {
+    // initialize values of min, max if it is first run
     if (firstRun) {
       min = num;
       max = num;
+      // then change to false
       firstRun = false;
     }
     // comparisons for max and min
@@ -52,23 +57,26 @@ int main(int argc, char** argv) {
     temperatures.push_back(num);
   }
 
-  float average = (float) sum / count;
-  float stdDev = calculateStandardDeviation(temperatures, average);
+  double average = (double) sum / count;
+  double stdDev = calculateStandardDeviation(temperatures, average);
 
   cout << "------------" << endl;
   cout << "count: " << count << endl;
   cout << "range: " << min << " - " << max << endl;
   cout << "mean: " << average << endl;
-  cout << "std_dev: " << stdDev << endl;
+  cout << "std_dev (sample): " << stdDev << endl;
   cout << "------------" << endl;
 
   // close the stream
   input.close();
+
+  printHistogram(temperatures, min, max);
+
   return 0;
 
 }
 
-float calculateStandardDeviation(vector<int> &items, float mean) {
+double calculateStandardDeviation(vector<double> &items, double mean) {
   int itemCount = items.size();
   
   if (itemCount == 1) {
@@ -76,11 +84,61 @@ float calculateStandardDeviation(vector<int> &items, float mean) {
     return -1.0;
   }
 
-  float sumOfDifferences = 0;
+  double sumOfDifferences = 0;
   
   for (int i = 0; i < itemCount; i++) {
-    sumOfDifferences += pow(float(items[i]) - mean, 2);
+    // cout << double(items[i]) << " minus " << mean << " equals " << ((double) items[i]) - mean << endl;
+    sumOfDifferences += pow(double(items[i]) - mean, 2);
+  }
+  return sqrt(1/((double)itemCount-1) * sumOfDifferences);
+}
+
+void printHistogram(vector<double> &items, double min, double max) {
+  int binCount = 20;
+  int binItems[20] = { 0 }; // all values set to 0
+  
+  // an array for start and end of each bin
+  double breakPoints[21] = { 0 };
+  double jumpSize = ((double)max - min) / binCount;
+  
+  // set the break point values eg. 70, 70.2, 70.4, ...
+  // of size binCount + 1
+  for (int i = 0; i < binCount + 1; i++) {
+    breakPoints[i] = min + i * jumpSize;
   }
 
-  return sqrt((float)1/(itemCount-1) * sumOfDifferences);
+  cout << "------------------" << endl;
+  cout << "    Histogram     " << endl;
+  cout << "------------------" << endl;
+
+  // assuming that the right hand side is NOT included in the limit
+  int itemsSize = items.size();
+  for (int i = 0; i < itemsSize; i++) {
+    double diff = items[i] - min;
+    int solveForMax = diff / jumpSize;
+    if (solveForMax == 0) {
+      binItems[solveForMax] += 1;
+    } else if (solveForMax == binCount) {
+      binItems[19] += 1;
+    } else {
+      binItems[solveForMax] += 1;
+    }
+  }
+
+  // print histogram with 'x' for values...
+  for (int i = 0; i < 20; i++) {
+    cout << setw(6) << breakPoints[i] << " - " << setw(6) << breakPoints[i+1] << ":" << '\0';
+    cout << string(binItems[i], '*') << endl;
+  }
+  cout << "------------------" << endl;
+
 }
+
+// void printArr(double arr[], int size);
+// void printArr(double arr[], int size) {
+//   cout << "------------------" << endl;
+//   for (int i = 0; i < size; i++) {
+//     cout << arr[i] << ' ';
+//   }
+//   cout << "\n------------------" << endl;
+// }
