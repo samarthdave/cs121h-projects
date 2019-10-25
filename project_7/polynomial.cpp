b471d10ddf8be53238d84d0d1fe149a720a6c8bb
Samarth Dave <samarthdave@users.noreply.github.com>
2019-10-25 09:42:51 -0500

    Initialize project 7 & update ignore with makefile

---
+++ project_7/polynomial.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // not expecting any command line input...
+  cout << "-----------------------" << endl;
+  cout << "       Polynomial      " << endl;
+  cout << "-----------------------" << endl;
+
+  return 0;
+}
